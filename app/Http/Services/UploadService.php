8fa7fe0ae63e9c7c2fc73d2605f9905cd462e406
André Fontoura <andd.fontoura@hotmail.com>
2022-10-05 11:08:13 -0300

    Acerta nome do arquivo

--- app/Http/Services/UploadService.php
+++ app/Http/Services/UploadService.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
 
 class UploadService {
-    public function uploadFileToFolder(string $disk = 'public', string $folderPath, array $file)
+    public function uploadFileToFolder(string $disk = 'public', string $folderPath, object $file)
     {
         return Storage::disk($disk)->put($folderPath, $file);
     }
